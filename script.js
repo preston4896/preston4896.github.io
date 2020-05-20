3b01e2cdb1360b36988836ff6e87c42337660bb1
Preston <preston4896@me.com>
2020-05-20 11:47:50 -0700

    Smooth scrolling and navbar jquery

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
 // scrollMagic code
 var controller = new ScrollMagic.Controller();
 var scene = new ScrollMagic.Scene();
+
+// navBar items active status code.
+// Code snippet: https://stackoverflow.com/questions/49530497/how-do-i-make-bootstrap-navbar-change-active-state
+$(document).ready(function() {
+    $( ".ml-auto .nav-item" ).bind( "click", function(event) {
+        var clickedItem = $( this );
+        $( ".ml-auto .nav-item" ).each( function() {
+            $( this ).removeClass( "active" );
+        });
+        clickedItem.addClass( "active" );
+    });
+});
