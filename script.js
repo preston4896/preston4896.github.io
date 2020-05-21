6c46f9d50647307ecbadbf665cfbedb6b6a1c5d6
Preston <preston4896@me.com>
2020-05-20 23:13:23 -0700

    i am done

--- script.js
+++ script.js
@@ -4,28 +4,38 @@ const animation1 = gsap.timeline();
 animation1.to('#headshot-div', {
     duration: 1, 
     y: 300,
-    opacity: 0
+    opacity: 0,
+    ease: "power4.out"
 });
 animation1.to('#text-div', {
     duration: 1,
     y: 300,
-    opacity: 0
+    opacity: 0,
+    ease: "power4.out"
 })
 animation1.to('#bg', {
     duration: 1,
     opacity: 0,
     ease: "circ.out"
 })
-animation1.to('#view2', {
+animation1.to('#bitmoji', {
     duration: 1,
-    opacity: 1
+    opacity: 1,
+})
+animation1.to('#v2-title', {
+    duration: 1,
+    opacity: 1,
+})
+animation1.to('#v2-text', {
+    duration: 1,
+    opacity: 1,
 })
 
 const controller = new ScrollMagic.Controller();
 const scene1 = new ScrollMagic.Scene({
     triggerElement: "#view2",
     duration: 500,
-    triggerHook: 0.25
+    triggerHook: 0.6
 })
 .setTween(animation1)
 .setPin("#view2")
