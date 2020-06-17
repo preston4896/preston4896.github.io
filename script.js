bfce0bfc9b34f693626222a654c07e31795338f5
Preston <preston4896@me.com>
2020-06-17 12:08:55 -0700

    more changes.

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
 "strict mode";
 
+// const animation1 = gsap.timeline();
+// animation1.to('#headshot-div', {
+//     duration: 1, 
+//     y: 300,
+//     opacity: 0,
+//     ease: "power4.out"
+// });
+// animation1.to('#text-div', {
+//     duration: 1,
+//     y: 300,
+//     opacity: 0,
+//     ease: "power4.out"
+// })
+// animation1.to('#view1', {
+//     duration: 1,
+//     opacity: 1,
+//     ease: "circ.out"
+// })
+// animation1.to('#bitmoji', {
+//     duration: 1,
+//     opacity: 1,
+// })
+// animation1.to('#v2-title', {
+//     duration: 1,
+//     opacity: 1,
+// })
+// animation1.to('#v2-text', {
+//     duration: 1,
+//     opacity: 1,
+// })
+
+// const controller = new ScrollMagic.Controller();
+// const scene1 = new ScrollMagic.Scene({
+//     triggerElement: "#view2",
+//     duration: 500,
+//     triggerHook: 0.5
+// })
+// .setTween(animation1)
+// .setPin("#view2")
+// .addIndicators()
+// .addTo(controller)
+
+// Animation: View 1
+// define timeline and animation
 const animation1 = gsap.timeline();
-animation1.to('#headshot-div', {
-    duration: 1, 
-    y: 300,
-    opacity: 0,
-    ease: "power4.out"
-});
-animation1.to('#text-div', {
+
+// move the headshot to the left.
+animation1.to("#headshot-div", {
     duration: 1,
-    y: 300,
-    opacity: 0,
-    ease: "power4.out"
-})
-animation1.to('#view1', {
-    duration: 1,
-    opacity: 1,
-    ease: "circ.out"
-})
-animation1.to('#bitmoji', {
-    duration: 1,
-    opacity: 1,
-})
-animation1.to('#v2-title', {
-    duration: 1,
-    opacity: 1,
-})
-animation1.to('#v2-text', {
-    duration: 1,
-    opacity: 1,
+    x: -150,
+    // opacity: 0,
+    ease: "power4-out"
 })
 
-const controller = new ScrollMagic.Controller();
-const scene1 = new ScrollMagic.Scene({
-    triggerElement: "#view2",
-    duration: 500,
-    triggerHook: 0.5
+// move the text to the right
+animation1.to("#text-div", {
+    duration: 1,
+    x: 150,
+    // opacity: 0,
+    ease: "power4-out"
 })
-.setTween(animation1)
-.setPin("#view2")
+
+// view 1 controller - bind the animations to the view.
+const controller1 = new ScrollMagic.Controller();
+const scene1 = new ScrollMagic.scene({
+    triggerElement: "#view1"
+})
 .addIndicators()
-.addTo(controller)
+.setTween(animation1)
+.setPin("#view1")
+.addTo(controller1)
