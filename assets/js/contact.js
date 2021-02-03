f88c3bd491088cb2f52348f58c141b858f782b2b
Preston Ong <preston4896@me.com>
2021-02-03 23:31:15 +0800

    Reorganized db structure.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,17 +1,11 @@
 "use strict";
 
-import {insertMessage} from "./firebase.js";
+import {insertMessage} from "./db.js";
 
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let subject = document.getElementById("subject");
 let message = document.getElementById("message");
-
-// error messages
-let nameErr = document.getElementById("name-error");
-let emailErr = document.getElementById("email-error");
-let subjectErr = document.getElementById("subject-error");
-let messageErr = document.getElementById("message-error");
 
 // submit event listener
 let contactForm = document.getElementById("contactForm");
@@ -24,10 +18,6 @@ clearBtn.addEventListener("click", clear);
 function submit() {
 
     if (name.checkValidity() && email.checkValidity() && subject.checkValidity() && message.checkValidity()) {
-        nameErr.innerHTML = "";
-        emailErr.innerHTML = "";
-        subjectErr.innerHTML = "";
-        messageErr.innerHTML = "";
         document.getElementById("status").innerHTML = `Thank you for your message, ${name.value}! I will get back to you ASAP. 😎`;
         
         // insert message to Firebase.
@@ -35,24 +25,6 @@ function submit() {
     }
     else {
         document.getElementById("status").innerHTML = "";
-        
-        // custom invalidation messages.
-        if (!name.checkValidity()) {
-            let error = "Oh nah nah! What's your name?";
-            nameErr.innerHTML = error.fontcolor("red");
-        }
-        if (!email.checkValidity()) {
-            let error = "Please enter a valid email.";
-            emailErr.innerHTML = error.fontcolor("red");
-        }
-        if (!subject.checkValidity()) {
-            let error = "Uhhh.... what's the agenda?";
-            subjectErr.innerHTML = error.fontcolor("red");
-        }
-        if (!message.checkValidity()) {
-            let error = "I am not a mind reader, please insert details.";
-            messageErr.innerHTML = error.fontcolor("red");
-        }
     }
 }
 
@@ -62,8 +34,4 @@ function clear() {
     email.value = "";
     subject.value = "";
     message.value = "";
-    nameErr.innerHTML = "";
-    emailErr.innerHTML = "";
-    subjectErr.innerHTML = "";
-    messageErr.innerHTML = "";
 }
---
+++ assets/js/db.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCegTT8bMekYwUMOh5qmLcbeeS6aElCncU",
+    authDomain: "prestonong-db.firebaseapp.com",
+    projectId: "prestonong-db",
+    storageBucket: "prestonong-db.appspot.com",
+    messagingSenderId: "619244013693",
+    appId: "1:619244013693:web:b9a7c61eb25e8654f90914",
+    measurementId: "G-G2X5EJD0LM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
+
+function insertMessage(name, email, subject, messageBody) {
+    let entryObj =
+        {
+            name: name,
+            email: email,
+            subject: subject,
+            message: messageBody
+        }
+    let collection = unixTime_to_date_convertor(Date.now());
+    db.collection(collection).add(entryObj).then((docRef) => {console.log("Doc ID: " + docRef.id)})
+    .catch((error) => {console.error(error)})
+};
+
+/**-------------------------------------------------------
+HELPER FUNCTIONS BELOW.
+---------------------------------------------------------*/
+
+/**
+ * Converts the UNIX timestamp to current month and year.
+ * @param {number} unix - milliseconds elapsed since January 1, 1970, 00:00:00 UTC
+ * @returns {string} - a date that only shows month and year. (For example: Sep 2021)
+ */
+function unixTime_to_date_convertor(unix) {
+    let unixDate = new Date(unix);
+    let year = unixDate.getFullYear();
+    let m = unixDate.getMonth();
+    let month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    return `${month[m]} + ${year}`;
+}
+
+export {insertMessage};
